584a5a399badc14864584cf993b5e4f5fb59339a
Dumitru Vulpe <dumitru.v.dv@gmail.com>
2020-01-15 21:03:56 +0000

    Added all of the authentication logic

--- db/models/user.model.js
+++ db/models/user.model.js
@@ -1,4 +1,9 @@
 const mongoose = require('mongoose');
+const _ = require('lodash');
+const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
+const bcrypt = require('bcryptjs');
+const jwtConfig = require('../../config/config').jwt;
 
 const UserSchema = new mongoose.Schema({
     username: {
@@ -24,6 +29,124 @@ const UserSchema = new mongoose.Schema({
         }
     }]
 });
+
+UserSchema.pre('save', function (next) {
+    let user = this;
+    let constFactor = 10;
+
+    if (user.isModified('password')) {
+        bcrypt.genSalt(constFactor, (err, salt) => {
+            bcrypt.hash(user.password, salt, (err, hash) => {
+                user.password = hash;
+                next();
+            });
+        });
+    } else {
+        next();
+    }
+});
+
+UserSchema.methods.toJSON = function () {
+    const user = this;
+    const userObject = user.toObject();
+
+    return _.omit(userObject, ['password', 'sessions']);
+}
+
+UserSchema.methods.generateAccessAuthToken = function () {
+    const user = this;
+    return new Promise((resolve, reject) => {
+        jwt.sign({ _id: user._id.toHexString() },
+            jwtConfig.secret,
+            { expiresIn: jwtConfig.expiresIn },
+            (err, token) => {
+                if (!err) {
+                    resolve(token);
+                } else {
+                    reject();
+                }
+            });
+    });
+}
+
+UserSchema.methods.generateRefreshAuthToken = function () {
+    return new Promise((resolve, reject) => {
+        crypto.randomBytes(64, (err, buff) => {
+            if (err) return reject(err);
+            let token = buff.toString('hex');
+            return resolve(token);
+
+        });
+    });
+}
+
+
+UserSchema.methods.createSession = function () {
+    let user = this;
+    return user.generateRefreshAuthToken().then((refreshToken) => {
+        return saveSessionToDatabase(user, refreshToken);
+    }).then((refreshToken) => {
+        return refreshToken;
+    }).catch((e) => {
+        return Promise.reject(`[AUTH] failed to save session to db\n ${e}`)
+    });
+
+}
+
+UserSchema.statics.findByIdAndToken = function (_id, token) {
+    let user = this;
+    return user.findOne({ _id, 'session.token': token });
+}
+
+UserSchema.statics.findByCredentials = function (username, password) {
+    let User = this;
+    return User.findOne({ username }).then((user) => {
+        if (!user) {
+            return Promise.reject();
+        }
+
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (err, res) => {
+                if (res) {
+                    resolve(user);
+                } else {
+                    reject();
+                }
+            });
+        })
+    });
+}
+
+UserSchema.statics.hasRefreshTokenExpired = (expiresAt) => {
+    let secondsSinceEpoc = Date.now() / 1000;
+    if (expiresAt > secondsSinceEpoc) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+UserSchema.statics.getJWTSecret = () => {
+    return jwtSecret;
+}
+
+let saveSessionToDatabase = (user, refreshToken) => {
+    return new Promise((resolve, reject) => {
+        let expiresAt = generateRefreshTokenExpiryTime();
+        user.sessions.push({ 'token': refreshToken, expiresAt });
+        user.save().then(() => {
+            resolve(refreshToken);
+        }).catch((err) => {
+            reject(err)
+        });
+    });
+}
+
+let generateRefreshTokenExpiryTime = () => {
+    let daysUntilExpire = jwtConfig.refreshTokenExpiery;
+    let secondsUntilExpie = ((daysUntilExpire * 24) * 60) * 60;
+    return ((Date.now() / 1000) + secondsUntilExpie);
+}
 
 const User = mongoose.model('Users', UserSchema);
 module.exports = { User };
