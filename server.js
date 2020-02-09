6d9c9a9bb33baa260faca607542cdd626127720d
Dumitru Vulpe <dumitru.v.dv@gmail.com>
2020-02-09 10:28:17 +0000

    Added a server class in a separate file

---
+++ server.js
@@ -0,0 +1,29 @@
+const express  = require('express');
+
+const IndexRoutes = require('./routes/index.routes');
+const appConfig = require('./config/config').app;
+
+const app = express();
+
+class Server {
+    constructor(db) {
+        this.setDb(db);
+        this.initRoutes();
+        this.start();
+    }
+
+    setDb(db) {
+        this.db = db;
+    }
+
+    start() {
+        app.listen(appConfig.port, () => console.log(`Portfolio-backend listening on port ${appConfig.port}!`));
+    }
+
+    initRoutes() {
+        let indexRoutes = new IndexRoutes(this.db);
+        app.use('/',indexRoutes.router);
+    }
+}
+
+module.exports = Server;
