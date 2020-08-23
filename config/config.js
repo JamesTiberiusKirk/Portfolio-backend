5b869b6f8da791d77da471b4f3ddffe4799024a4
Dumitru Vulpe <dumitru.v.dv@gmail.com>
2020-08-23 22:10:48 +0100

    Added dotenv

--- config/config.js
+++ config/config.js
@@ -1,23 +1,33 @@
-function getCreds(){
-    return process.env.CREDS;
+require('dotenv').config();
+
+function getDbConfig() {
+    return {
+        creds: process.env.DB_CREDS,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dbName: process.env.DB_NAME
+    };
+}
+
+function getAppConfig() {
+    return {
+        port: process.env.API_PORT
+    };
+}
+
+function getJwtConfig() {
+    return {
+        expiresIn: process.env.JWT_EXPIRATION,
+        secret: process.env.JWT_SECRET,
+        refreshTokenExpiry: process.env.JWT_REFRESH_TOKEN_EXPIRY,
+        constFactor: process.send.JWT_CONST_FACTOR
+    };
 }
 
 const config = {
-    app: {
-        port: 3000
-    },
-    db: {
-        creds: getCreds(),
-        host: 'localhost',
-        // host: 'portfolio-db',
-        port: 27017,
-        dbName: 'portfolio'
-    },
-    jwt: {
-        expiresIn: '15m',
-        secret: 'L5Zp3INaAkPTtdLH7RTNwQtoXyfMfoZxNVX6IOgu', //temporary
-        refreshTokenExpiery: '10' //days
-    }
+    app: getAppConfig(),
+    db: getDbConfig(),
+    jwt: getJwtConfig(),
 }
 
 module.exports = config;
